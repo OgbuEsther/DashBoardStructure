0bcaf5cf47d69b78c864239d3bb54a7b815a83e4
Ogbu Esther <ogbuuzoma413@gmail.com>
2023-03-13 12:13:02 +0100

    saved routings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import HomeRoutes from "./Components/Routes/HomeRoutes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <HomeRoutes />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Routes/HomeRoutes.tsx
+++ src/Components/Routes/HomeRoutes.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useRoutes } from "react-router-dom";
+import HomeScreen from "../HomeScreen";
+import Footer from "../LandingPage/Footer";
+import Header from "../LandingPage/Header";
 
 const HomeRoutes = () => {
-  return <div>HomeRoutes</div>;
+  let elements = useRoutes([
+    {
+      path: "/",
+      element: <HomeScreen />,
+    },
+  ]);
+  return (
+    <div>
+      <Header />
+      {elements}
+      <Footer />
+    </div>
+  );
 };
 
 export default HomeRoutes;
